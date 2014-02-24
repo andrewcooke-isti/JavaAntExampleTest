1cd5ef89a9d6432dadb803e53d98336b67d4a7ee
andrew cooke <a.cooke@isti.com>
2014-02-24 11:48:55 -0300

    bad test

--- src/test/java/com/isti/cats/ExampleTest.java
+++ src/test/java/com/isti/cats/ExampleTest.java
@@ -11,4 +11,10 @@ public class ExampleTest {
 	assert e.getValue().equals("foo");
     }
 
+    @Test
+    public void testValue2() {
+	Example e = new Example("foo");
+	assert e.getValue().equals("bar");
+    }
+
 }
