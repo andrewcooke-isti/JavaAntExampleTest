9ba53f792389e00361ab3b58905255f8462574a9
andrew cooke <a.cooke@isti.com>
2014-02-24 16:01:17 -0300

    bad test

--- src/test/java/com/isti/cats/ExampleTest.java
+++ src/test/java/com/isti/cats/ExampleTest.java
@@ -2,19 +2,22 @@
 package com.isti.cats;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
     @Test
     public void testValue() {
 	Example e = new Example("foo");
-	assert e.getValue().equals("foo");
+	assertEquals(e.getValue(), "foo");
     }
 
     @Test
     public void testValue2() {
 	Example e = new Example("foo");
-	assert e.getValue().equals("bar");
+	System.out.println(e.getValue());
+	System.out.println(e.getValue().equals("bar"));
+	assertEquals(e.getValue(), "bar");
     }
 
 }
